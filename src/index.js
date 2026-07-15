f72e38c30a5ff22496bb4363735a17637980d925
tom20011 <gogo3b1b@outlook.com>
2026-07-15 16:18:57 +0800

    Add handling for '/live/all.m3u' path

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 import { handle as fixOrigin } from './fix_origin.js';
 import { handle as fixEpg } from './fix_epg.js';
 import { handle as cacheM3u } from './cache_m3u.js';
+import { handle as allM3u } from './all_m3u.js';
 
 export default {
   async fetch(request, env, ctx) {
     const url = new URL(request.url);
     const path = url.pathname;
+
+    if (path === '/live/all.m3u') {
+      return allM3u(request, env, ctx);
+    }
 
     if (path === env.PANDALIVE_PATH) {
       return fixOrigin(request, env, ctx);
